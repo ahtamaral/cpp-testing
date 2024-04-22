d1a3a4c33641e3fec140b005b6f91ad912af5bf7
Artur Amaral <artur.amaral@mettainnovations.com.br>
2024-04-22 19:42:57 -0300

    New tests.

--- tests/test.cpp
+++ tests/test.cpp
@@ -2,17 +2,46 @@
 #include "../Calculator/src/arithmetics/operations.h"
 #include <random>
 #include <limits>
+#include <cfloat>
 
-TEST(IntSumTest, SumCorrectly) 
+TEST(SumTest, IntSumCorrectly) 
 {
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<int> dist(std::numeric_limits<int>::min(), std::numeric_limits<int>::max());
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 1000; ++i) {
         // Generate random integers a and b
         int a = dist(gen);
         int b = dist(gen);
+        ASSERT_EQ(a+b, sum(a,b));
+    }
+}
+
+TEST(SumTest, FloatSumCorrectly) 
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<float> dist(FLT_MIN, FLT_MAX);
+
+    for (int i = 0; i < 1000; ++i) {
+        // Generate random integers a and b
+        float a = dist(gen);
+        float b = dist(gen);
+        ASSERT_EQ(a+b, sum(a,b));
+    }
+}
+
+TEST(SumTest, DoubleSumCorrectly) 
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(DBL_MIN, DBL_MAX);
+
+    for (int i = 0; i < 1000; ++i) {
+        // Generate random integers a and b
+        float a = dist(gen);
+        float b = dist(gen);
         ASSERT_EQ(a+b, sum(a,b));
     }
 }
